d0400703ff8af6419c00ab5ca05ee4e097916c4b
Dominik Jakubowski <dominikjakubowski@gmail.com>
2017-12-03 19:14:04 +0100

    Cat created

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 75;
+        int y = 79;
+        int z = 84;
+        char a = (char)x;
+        char b = (char)y;
+        char c = (char)z;
+
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+    }
+}
